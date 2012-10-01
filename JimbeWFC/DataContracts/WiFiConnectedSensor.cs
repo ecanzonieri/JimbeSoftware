cfc7c9c40a2400a55a40d2a61316eeef859e57f7
Enrico Canzonieri <ecanzonieri@gmail.com>
2012-10-01 21:30:44 +0200

    JimbeService - Solved OnStart Bug and Added new project JimbeServiceTestLikeApplication in order to test the service.

--- JimbeWFC/DataContracts/WiFiConnectedSensor.cs
+++ JimbeWFC/DataContracts/WiFiConnectedSensor.cs
@@ -3,6 +3,7 @@ using System.Runtime.Serialization;
 
 namespace JimbeWFC.DataContracts
 {
+    [KnownType(typeof(List<WiFiNetwork>))]
     [DataContract (Name = "Sensor")]
     public class WiFiConnectedSensor : Sensor
     {
