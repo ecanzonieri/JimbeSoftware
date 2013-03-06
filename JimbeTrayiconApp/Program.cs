7455baaf118b998c93c6cbaf737d53adc2cf5106
emanuelej <emanuelefurci@gmail.com>
2013-03-06 13:13:09 +0100

    JimbeApp- First Restyle of gui.
    JimbeTrayiconApp- Realized tray-icon application that let the user activate and deactivate the service.

---
+++ JimbeTrayiconApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JimbeTrayiconApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        // per far partire un nuovo processo...
+        //  System.Diagnostics.Process.Start(new System.Diagnostics.Process...strartInfo)
+
+        }
+    }
+}
