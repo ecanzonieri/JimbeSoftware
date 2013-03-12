96ef0594f30e7c0e0b60cfa4f038a57f0ecc36fb
emanuelej <emanuelefurci@gmail.com>
2013-03-12 01:34:39 +0100

    -TrayIconApp
    ° Bugs Fixed: the application now run without errors.
    
    ° Icon images unified: added icon of same type.
    
    ° Added intermediate state during activation and deactivation of service (in this state the menu option related on the service is disabled).

--- JimbeTrayiconApp/Form1.cs
+++ JimbeTrayiconApp/Form1.cs
@@ -27,7 +27,7 @@ namespace JimbeTrayiconApp
             catch (Exception)
             {
                 MessageBox.Show("Servizio non trovato, assicurati di averlo installato");
-                notifyIcon1.Icon = Properties.Resources.serviceOff;
+                notifyIcon1.Icon = Properties.Resources.flag_red;
                 Application.Exit();
             }                       
             
@@ -43,7 +43,7 @@ namespace JimbeTrayiconApp
                {
                    attivatoreToolStripMenuItem.Text = "Attiva LocalizeMe";
                    locationName = "LocalizeMe non attivo";
-                   notifyIcon1.Icon = Properties.Resources.serviceOff;
+                   notifyIcon1.Icon = Properties.Resources.flag_red;
                    notifyIcon1.Text = locationName;
                }               
                 actualLocation = _proxy.GetCurrentLocation();
@@ -52,14 +52,14 @@ namespace JimbeTrayiconApp
                {
                    //locazione non trovata
                    locationName = "nessuna locazione individuata";
-                   notifyIcon1.Icon = Properties.Resources.serviceOn;
+                   notifyIcon1.Icon = Properties.Resources.flag_blue;
                    notifyIcon1.Text = locationName;
                }
                else
                {
                    //locazione trovata
                    locationName = actualLocation.Name;
-                   notifyIcon1.Icon=Properties.Resources.locationFound;
+                   notifyIcon1.Icon = Properties.Resources.flag_green;
 
                }
            }
@@ -67,7 +67,7 @@ namespace JimbeTrayiconApp
                {
                    attivatoreToolStripMenuItem.Text = "Attiva LocalizeMe";
                    locationName = "LocalizeMe non attivo";
-                   notifyIcon1.Icon = Properties.Resources.serviceOff;
+                   notifyIcon1.Icon = Properties.Resources.flag_red;
                }
         }
 
@@ -80,18 +80,26 @@ namespace JimbeTrayiconApp
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            jimbeService = new ServiceController("JimbeService");
+            _proxy = ProxyFactory.GetProxy(Properties.Settings.Default.Url_TryApp);
+
             //controlla che il servizio è attivo e prende la locazione attuale aggiornando l'icona come serve
-            if (jimbeService.Status == ServiceControllerStatus.Running)
+            if (jimbeService.Status != ServiceControllerStatus.StopPending && jimbeService.Status != ServiceControllerStatus.Stopped)
            {
                attivatoreToolStripMenuItem.Text = "Disattiva LocalizeMe";
                //servizio attivo
                //mi collego al servizio tramite wcf per ottenere la locazione attuale
-               actualLocation = _proxy.GetCurrentLocation();
+               try
+               {
+                   actualLocation = _proxy.GetCurrentLocation();
+               }
+                catch(Exception){}
+
                if (actualLocation == null)
                {
                    //locazione non trovata
                    locationName = "nessuna locazione individuata";
-                   notifyIcon1.Icon = Properties.Resources.serviceOn;
+                   notifyIcon1.Icon = Properties.Resources.flag_blue;
                    notifyIcon1.Text = locationName;
 
                }
@@ -99,7 +107,7 @@ namespace JimbeTrayiconApp
                {
                    //locazione trovata
                    locationName = actualLocation.Name;
-                   notifyIcon1.Icon=Properties.Resources.locationFound;
+                   notifyIcon1.Icon = Properties.Resources.flag_green;
                    notifyIcon1.Text = locationName;
                }
            }
@@ -107,28 +115,34 @@ namespace JimbeTrayiconApp
                {
                    attivatoreToolStripMenuItem.Text = "Attiva LocalizeMe";
                    locationName = "LocalizeMe non attivo";
-                   notifyIcon1.Icon = Properties.Resources.serviceOff;
+                   notifyIcon1.Icon = Properties.Resources.flag_red;
                    notifyIcon1.Text = locationName;
                }
+            attivatoreToolStripMenuItem.Enabled = true;
+
         }
 
         private void attivatoreToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //attiva e dissattiva il servizio
-            if (jimbeService.Status == ServiceControllerStatus.Running)
+            if (jimbeService.Status != ServiceControllerStatus.StopPending && jimbeService.Status != ServiceControllerStatus.Stopped)
             {
                 try
                 {
                     jimbeService.Stop();
                 }
-                catch (Exception)
+                catch (InvalidOperationException)
                 {
+               
+                }
+                catch(Exception){
                     MessageBox.Show("Errore durante lo stop del servizio, l'applicazione verrà chiusa");
                     Application.Exit();
                 }
-                notifyIcon1.Icon = Properties.Resources.serviceOff;
-                notifyIcon1.Text = "LocalizeMe non attivo";
+                notifyIcon1.Icon = Properties.Resources.flag_yellow;
+                notifyIcon1.Text = "Disattivazione in corso";
                 attivatoreToolStripMenuItem.Text = "Attiva LocalizeMe";
+                attivatoreToolStripMenuItem.Enabled = false;
 
             }
             else
@@ -137,14 +151,19 @@ namespace JimbeTrayiconApp
                 {
                     jimbeService.Start();
                 }
+                catch (InvalidOperationException)
+                {
+
+                }
                 catch (Exception)
                 {
-                    MessageBox.Show("Errore durante l'avvio del servizio, l'applicazione verrà chiusa");                   
+                    MessageBox.Show("Errore durante lo stop del servizio, l'applicazione verrà chiusa");
                     Application.Exit();
                 }
-                notifyIcon1.Icon = Properties.Resources.serviceOn;
-                notifyIcon1.Text = "nessuna locazione individuata";
+                notifyIcon1.Icon = Properties.Resources.flag_yellow;
+                notifyIcon1.Text = "Attivazione in corso";
                 attivatoreToolStripMenuItem.Text = "Disattiva LocalizeMe";
+                attivatoreToolStripMenuItem.Enabled = false;
 
             }
         }
